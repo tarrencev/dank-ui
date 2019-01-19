1c1e7cbb65e5ef8ca2dcdf1fe8249deacf41ceef
Tarrence van As <tarrence13@gmail.com>
2019-01-18 21:03:11 -0500

    Remove unused imports

--- src/index.js
+++ src/index.js
@@ -15,26 +15,6 @@ export { default as CardHeader } from "./components/card/CardHeader";
 export { default as CardSubtitle } from "./components/card/CardSubtitle";
 export { default as CardTitle } from "./components/card/CardTitle";
 
-export { default as BountyIcon } from "./components/icons/BountyIcon";
-export { default as CaretIcon } from "./components/icons/CaretIcon";
-export { default as CheckIcon } from "./components/icons/CheckIcon";
-export { default as CollateralIcon } from "./components/icons/CollateralIcon";
-export {
-  default as CommonInterestIcon
-} from "./components/icons/CommonInterestIcon";
-export { default as DisputeIcon } from "./components/icons/DisputeIcon";
-export { default as EmailIcon } from "./components/icons/EmailIcon";
-export { default as EthereumIcon } from "./components/icons/EthereumIcon";
-export { default as GithubIcon } from "./components/icons/GithubIcon";
-export { default as LinkedInIcon } from "./components/icons/LinkedInIcon";
-export { default as MetaMaskIcon } from "./components/icons/MetaMaskIcon";
-export { default as OverviewIcon } from "./components/icons/OverviewIcon";
-export { default as RedditIcon } from "./components/icons/BountyIcon";
-export { default as RepoIcon } from "./components/icons/RepoIcon";
-export { default as TelegramIcon } from "./components/icons/TelegramIcon";
-export { default as TokenIcon } from "./components/icons/TokenIcon";
-export { default as TwitterIcon } from "./components/icons/TwitterIcon";
-
 export { default as Input } from "./components/input/Input";
 
 export { default as InputRow } from "./components/input-row/InputRow";
@@ -79,14 +59,8 @@ export {
   default as EditorTabPanel
 } from "./components/text-editor/EditorTabPanel";
 export { default as TextEditor } from "./components/text-editor/TextEditor";
-export { default as TextEditor2 } from "./components/text-editor/TextEditor2";
 
 export { default as Textarea } from "./components/textarea/Textarea";
 
-export { default as TokenValue } from "./components/token/TokenValue";
-
-export { default as TokenInput } from "./components/token-input/TokenInput";
-
 // Utils
 export { default as adapt } from "./utils/adapt";
-export { default as withLoading } from "./utils/withLoading";
